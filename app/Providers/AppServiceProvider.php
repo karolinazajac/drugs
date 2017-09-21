a0b7e49768aee622dced00b501aff506e922fc21
karolinazajac <karolinazajac05@gmail.com>
2017-09-21 23:45:17 +0200

    app service provider fix for database keys length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use  Illuminate\Support\Facades\Schema;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -18,6 +19,7 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment() == 'production') {
             URL::forceScheme('https');
         }
+        Schema::defaultStringLength(191);
     }
 
     /**
