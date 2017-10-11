6e1e6aeb066c122f93d41ca08a61d0e6df3d483c
karolinazajac <karolinazajac05@gmail.com>
2017-10-12 00:40:19 +0200

    changed database credentials

--- config/database.php
+++ config/database.php
@@ -45,33 +45,15 @@ $database = substr($url["path"], 1),
             'prefix' => '',
         ],
         /**
-         * data for local environment
-         */
-//        'mysql' => [
-//            'driver' => 'mysql',
-//            'host' => env('DB_HOST', $host),
-//            'port' => env('DB_PORT', '3306'),
-//            'database' => env('DB_DATABASE', $database),
-//            'username' => env('DB_USERNAME', $username),
-//            'password' => env('DB_PASSWORD', $password),
-//            'unix_socket' => env('DB_SOCKET', ''),
-//            'charset' => 'utf8mb4',
-//            'collation' => 'utf8mb4_unicode_ci',
-//            'prefix' => '',
-//            'strict' => true,
-//            'engine' => null,
-//        ],
-        /**
          * data for heroku server
          */
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-//            'host' => env('DB_HOST', 'localhost') . ('homestead' == gethostname() ? null : ':33060'),
+            'host' => env('DB_HOST', $host),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', $database),
+            'username' => env('DB_USERNAME', $username),
+            'password' => env('DB_PASSWORD', $password),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -79,6 +61,24 @@ $database = substr($url["path"], 1),
             'strict' => true,
             'engine' => null,
         ],
+        /**
+         * data for local environment
+         */
+//        'mysql' => [
+//            'driver' => 'mysql',
+//            'host' => env('DB_HOST', '127.0.0.1'),
+////            'host' => env('DB_HOST', 'localhost') . ('homestead' == gethostname() ? null : ':33060'),
+//            'port' => env('DB_PORT', '3306'),
+//            'database' => env('DB_DATABASE', 'forge'),
+//            'username' => env('DB_USERNAME', 'forge'),
+//            'password' => env('DB_PASSWORD', ''),
+//            'unix_socket' => env('DB_SOCKET', ''),
+//            'charset' => 'utf8mb4',
+//            'collation' => 'utf8mb4_unicode_ci',
+//            'prefix' => '',
+//            'strict' => true,
+//            'engine' => null,
+//        ],
 
         'pgsql' => [
             'driver' => 'pgsql',
